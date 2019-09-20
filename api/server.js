64e839da1da3439b69ddef144ca587447ac6d7ae
Thai Nguyen <discoverylight@yahoo.com>
2019-09-20 10:56:27 -0700

    added rate-limiter

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const config = require("../config");
 const cors = require("cors");
 const helmet = require("helmet");
-
+const rateLimit = require("express-rate-limit");
 const authRouter = require("../auth/auth-router.js");
 const jokesRouter = require("../jokes/jokes-router.js");
 
 const server = express();
 
+server.use(rateLimit(config.requestRateLimit));
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
--- config/index.js
+++ config/index.js
@@ -1,4 +1,9 @@
 const config = {
+  requestRateLimit: {
+    windowMs: 1000 * 60 * 15,
+    max: 180,
+    message: "Too many requests"
+  },
   secrets: {
     jwt: " iloveJavaScript!",
     jwtExp: "1h"
